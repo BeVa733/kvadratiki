c8ce6b1926b37a89143cdcc2eb34b9e1ffc5650b
BeVa733 <vadim.belousov.2000@inbox.ru>
2025-08-15 15:53:06 +0500

    Add files via upload

--- kvadratiki2.0.cpp
+++ kvadratiki2.0.cpp
@@ -1,122 +1,139 @@
 #include<TXLib.h>
 #include<stdio.h>
 #include<math.h>
-int input_1(double *kef_a,double *kef_b,double *kef_c);
-void output_1(double sol1, double sol2, int cas);
-int solver(double kef_a,double kef_b,double kef_c, double *sol1, double *sol2);
+
+int input_coefficients (double *coefficient_a,double *coefficient_b,double *coefficient_c);
+void output_received_solution (double sol_1, double sol_2, int type_of_output);
+int the_quadratic_equation_solver (double coefficient_a,double coefficient_b,double coefficient_c, double *sol_1, double *sol_2);
+
 int main(void)
 {
-    double kef_a, kef_b, kef_c;
-    double sol1 = 0;
-    double sol2 = 0;
-    int pruf = 1;
-    int prov, cas;
-    printf("Р­С‚Р° РїСЂРѕРіСЂР°РјРјР° СЂРµС€Р°РµС‚ СѓСЂР°РІРЅРµРЅРёСЏ РІРёРґР° ax^2+bx+c=0\n");
+    double coefficient_a = 0;
+    double coefficient_b = 0;
+    double coefficient_c = 0;
+    double sol_1 = 0;
+    double sol_2 = 0;
+    int need_for_repetition = 1;
+    int check_correctness_input, type_of_output;
+    printf("Эта программа решает уравнения вида ax^2+bx+c=0\n");
+
     do
     {
-        prov = 1;
-        printf("Р’РІРµРґРёС‚Рµ РїР°СЂР°РјРµС‚СЂС‹ СѓСЂР°РІРЅРµРЅРёСЏ:\n");
-        prov = input_1(&kef_a, &kef_b, &kef_c);
-        if (prov == 1)
-            cas = solver(kef_a, kef_b, kef_c, &sol1, &sol2);
+        check_correctness_input = 1;
+        printf("Введите параметры уравнения:\n");
+        check_correctness_input = input_coefficients(&coefficient_a, &coefficient_b, &coefficient_c);
+
+        if (check_correctness_input == 1)
+            type_of_output = the_quadratic_equation_solver(coefficient_a, coefficient_b, coefficient_c, &sol_1, &sol_2);
         else
-            cas = -1;
-        output_1(sol1, sol2, cas);
-        printf("Р•СЃР»Рё С…РѕС‚РёС‚Рµ СЂРµС€РёС‚СЊ РµС‰Рµ РѕРґРЅРѕ СѓСЂР°РІРЅРµРЅРёРµ РІРІРµРґРёС‚Рµ 1, РґР»СЏ Р·Р°РІРµСЂС€РµРЅРёСЏ РїСЂРѕРіСЂР°РјРјС‹ РІРІРµРґРёС‚Рµ 0\n");
-        scanf("%d", &pruf);
-    }while(pruf);
+            type_of_output = -1;
+
+        output_received_solution(sol_1, sol_2, type_of_output);
+
+        printf("Если хотите решить еще одно уравнение введите 1, для завершения программы введите 0\n");
+        scanf("%d", &need_for_repetition);
+    }while(need_for_repetition);
+
     return 0;
 }
-int input_1(double *kef_a, double *kef_b, double *kef_c)
+
+
+int input_coefficients (double *coefficient_a, double *coefficient_b, double *coefficient_c)
 {
-    int prov = 1;
+    int check_correctness_input = 1;
     printf("a= ");
-    if (scanf("%lf", kef_a) != 1)
-        prov = 0;
+
+    if (scanf("%lf", coefficient_a) != 1)
+        check_correctness_input = 0;
     else
     {
         printf("b= ");
-        if (scanf("%lf", kef_b) != 1)
-            prov = 0;
+        if (scanf("%lf", coefficient_b) != 1)
+            check_correctness_input = 0;
         else
         {
             printf("c= ");
-            if (scanf("%lf", kef_c)!= 1)
-                prov = 0;
+            if (scanf("%lf", coefficient_c)!= 1)
+                check_correctness_input = 0;
         }
     }
-    if (prov == 0)
+
+    if (check_correctness_input == 0)
     {
         while(getchar() != '\n')
             continue;
     }
-    return prov;
+
+    return check_correctness_input;
 }
-int solver(double kef_a,double kef_b,double kef_c, double *sol1, double *sol2)
+
+
+int the_quadratic_equation_solver (double coefficient_a,double coefficient_b,double coefficient_c, double *sol_1, double *sol_2)
 {
-    double D;
-    int cas;
-    if (kef_a == 0)
+    double D = 0;
+    int type_of_output;
+
+    if (coefficient_a == 0)
     {
-        if (kef_b ==0)
+        if (coefficient_b ==0)
         {
-            if (kef_c == 0)
-                cas = 0;
+            if (coefficient_c == 0)
+                type_of_output = 0;
             else
-               cas = 1;
+               type_of_output = 1;
         }
         else
         {
-            cas = 2;
-            *sol1 = -kef_c/kef_b;
+            type_of_output = 2;
+            *sol_1 = -coefficient_c/coefficient_b;
         }
     }
     else
     {
-        D = kef_b*kef_b-4*kef_a*kef_c;
+        D = coefficient_b*coefficient_b-4*coefficient_a*coefficient_c;
         if (D<0)
-            cas = 3;
+            type_of_output = 3;
         else if (D==0)
         {
-            cas = 4;
-            *sol1 = -kef_b/(2*kef_a);
+            type_of_output = 4;
+            *sol_1 = -coefficient_b/(2*coefficient_a);
         }
         else
         {
-            cas = 5;
-            *sol1 = (-kef_b+sqrt(D))/(2*kef_a);
-            *sol2 = (-kef_b-sqrt(D))/(2*kef_a);
+            type_of_output = 5;
+            *sol_1 = (-coefficient_b+sqrt(D))/(2*coefficient_a);
+            *sol_2 = (-coefficient_b-sqrt(D))/(2*coefficient_a);
         }
     }
-    return cas;
+
+    return type_of_output;
 }
-void output_1(double sol1, double sol2, int cas)
+
+
+void output_received_solution (double sol_1, double sol_2, int type_of_output)
 {
-    switch(cas)
+    switch(type_of_output)
     {
         case -1:
-            printf("Р’РІРµРґРµРЅС‹ РЅРµРєРѕСЂСЂРµРєС‚РЅС‹Рµ РєРѕСЌС„С„РёС†РёРµРЅС‹\n");
+            printf("Введены некорректные коэффициены\n");
             break;
         case 0:
-            printf("Р­С‚Рѕ СѓСЂР°РІРЅРµРЅРёРµ РёРјРµРµС‚ Р±РµСЃРєРѕРЅРµС‡РЅРѕРµ РєРѕР»РёС‡РµСЃС‚РІРѕ СЂРµС€РµРЅРёР№\n");
+            printf("Это уравнение имеет бесконечное количество решений\n");
             break;
         case 1:
-            printf("СЌС‚Рѕ СѓСЂР°РІРЅРµРЅРёРµ РЅРµ РёРјРµРµС‚ СЂРµС€РµРЅРёР№\n");
+        case 3:
+            printf("это уравнение не имеет решений\n");
             break;
         case 2:
-            printf("Р­С‚Рѕ СѓСЂР°РІРЅРµРЅРёРµ РёРјРµРµС‚ РѕРґРЅРѕ СЂРµС€РµРЅРёРµ %.2f\n", sol1);
-            break;
-        case 3:
-            printf("СЌС‚Рѕ СѓСЂР°РІРЅРµРЅРёРµ РЅРµ РёРјРµРµС‚ СЂРµС€РµРЅРёР№\n");
-            break;
         case 4:
-            printf("Р­С‚Рѕ СѓСЂР°РІРЅРµРЅРёРµ РёРјРµРµС‚ РѕРґРЅРѕ СЂРµС€РµРЅРёРµ %.2f\n", sol1);
+            printf("Это уравнение имеет одно решение %.2f\n", sol_1);
             break;
+
         case 5:
-            printf("Р­С‚Рѕ СѓСЂР°РІРЅРµРЅРёРµ РёРјРµРµС‚ 2 СЂРµС€РµРЅРёСЏ x=%.2f Рё x=%.2f\n", sol1, sol2);
+            printf("Это уравнение имеет 2 решения x=%.2f и x=%.2f\n", sol_1, sol_2);
             break;
         default:
-            printf("Р§С‚Рѕ-С‚Рѕ РїРѕР»РѕРјР°Р»РѕСЃСЊ.....\n");
+            printf("Что-то поломалось.....\n");
             break;
     }
 }
